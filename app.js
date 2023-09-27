e323716a7baac019adad3a90c2807f61c50a8442
makkiraza <makkirazaa@gmail.com>
2023-09-28 03:21:49 +0500

    hogya

---
+++ app.js
@@ -0,0 +1,11 @@
+function abc(e){
+    document.getElementById("inp").value += e
+}
+
+function eq(){
+    var a = document.getElementById('inp')
+    a.value = eval(a.value)
+}
+function clr(){
+    document.getElementById('inp').value = ''
+}
